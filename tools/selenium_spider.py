7f1a4c15c7693e8d1ba7cb7a1c556f6cd92dd5e1
zsdostar <zsdzzydos@gmail.com>
2018-01-03 22:59:08 +0800

    selenium.py: 用selenium完成了对知乎的模拟登录 zhihu_sel.py: 导入的Bobby的文件 chromedriver.exe: selenium的驱动

--- tools/selenium_spider.py
+++ tools/selenium_spider.py
@@ -11,9 +11,23 @@
 """
 __author__ = 'zsdostar'
 
-
 from selenium import webdriver
+from scrapy.selector import Selector
 
-browser = webdriver.Chrome(executable_path='chromedriver.exe')  # 这个exe是selenium的Chrome驱动
-browser.get('https://www.baidu.com')
-print browser.page_source
+
+browser = webdriver.Chrome(executable_path='../ArticleSpider/spiders/chromedriver.exe')  # 这个exe是selenium的Chrome驱动
+
+browser.get('https://www.zhihu.com/')# 打开浏览器及其网页
+# print browser.page_source  # 打印F12的HTML代码
+
+# 模拟输入用户名密码
+browser.find_element_by_css_selector(".Input-wrapper input[name='username']").send_keys('18846030787')
+browser.find_element_by_css_selector(".Input-wrapper input[name='password']").send_keys('klxsxzsdf1')
+# 模拟点击
+browser.find_element_by_css_selector(".SignFlow-submitButton").click()
+
+t_seletor = Selector(text=browser.page_source)  # 将整个HTML传到Scrapy的selector里
+# print t_seletor.css('.tm-promo-price .tm-price::text').extract()  # css选择器获取tmall价格
+
+
+browser.quit()  # 手动关浏览器是不好使的，还会在任务栏待很久
