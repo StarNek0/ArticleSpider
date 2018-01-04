5ef4414c29d7b276009cd9d8e362303e0f42bd86
zsdostar <zsdzzydos@gmail.com>
2018-01-04 14:51:56 +0800

    完成了selenium不加载图片访问网页（PS:ChromeDriver和其调用的配置变量一定要对应的上。如果不清楚变量名，就下载最新版的ChromeDriver，其配置变量名应该下载最新版的Chrome去看Chrome的配置文件，或者看官方文档）

--- tools/selenium_spider.py
+++ tools/selenium_spider.py
@@ -15,6 +15,18 @@ import time
 from selenium import webdriver
 from scrapy.selector import Selector
 
+# 设置ChromeDriver不加载图片
+# 为什么这么写，因为Chrome配置文件是这个，里面可以搜到变量名字 C:\Users\Administrator\AppData\Local\Google\Chrome\User Data\Default\Preferences
+chrome_opt = webdriver.ChromeOptions()
+prefs = {
+    "profile.default_content_setting_values":
+        {
+            "images":2,
+            # "javascript":2
+        }
+}
+chrome_opt.add_experimental_option("prefs", prefs)
+
 # 知乎
 # browser = webdriver.Chrome(executable_path='../ArticleSpider/spiders/chromedriver.exe')  # 这个exe是selenium的Chrome驱动
 #
@@ -34,7 +46,11 @@ from scrapy.selector import Selector
 # browser.quit()  # 手动关浏览器是不好使的，还会在任务栏待很久
 
 # 微博
-browser = webdriver.Chrome(executable_path='../ArticleSpider/spiders/chromedriver.exe')  # 这个exe是selenium的Chrome驱动
+browser = webdriver.Chrome(executable_path='../ArticleSpider/spiders/chromedriver.exe', chrome_options=chrome_opt)  # 这个exe是selenium的Chrome驱动
+# browser.get("https://www.taobao.com")
+# time.sleep(5)
+# browser.quit()
+
 try:
     browser.get('https://weibo.com')
     time.sleep(7)
@@ -42,8 +58,8 @@ try:
     browser.find_element_by_css_selector(".info_list.password input[name='password']").send_keys('klxsxzsdf1')
     time.sleep(1)
     browser.find_element_by_css_selector(".info_list.login_btn a[node-type='submitBtn']").click()
-    for i in xrange(5):
-        time.sleep(2)
+    for i in xrange(2):
+        time.sleep(3)
         browser.execute_script("window.scrollTo(0, document.body.scrollHeight);"
                            "var lenOfPage=document.body.scrollHeight;"
                            "return lenOfPage;")
